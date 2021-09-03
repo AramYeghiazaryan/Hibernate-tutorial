5bdfe7b5d9a03d67f9a063ba6dabd82b231f9a6e
AramYeghiazaryan <yeghiazaryan99@gmail.com>
2021-09-03 18:32:44 +0400

    fist steps

--- src/main/java/utils/EntityPersist.java
+++ src/main/java/utils/EntityPersist.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 
 public class EntityPersist<T> {
 
-
     public void persist(T entity) {
         Session session = SessionFactorySingleton.getSessionInstance().getCurrentSession();
         session.save(entity);
